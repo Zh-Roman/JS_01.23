50c8e20cc92d0cc262e906b03fd04f18fee88197
Roman Zhuravkevich <zhuravkevich.roman@gmail.com>
2023-02-01 19:32:27 +0300

    [1.2:01.02]: finished second task.

--- js/main.js
+++ js/main.js
@@ -20,3 +20,40 @@ const numberInfo = () => {
   }
 };
 numberInfo();
+
+const matrix = () => {
+  const matrixElementsFn = () => {
+    const matrixElements = prompt('Enter no more than 3 characters and no less than 1.');
+    if (matrixElements !== null) {
+      if (matrixElements.length < 1 ||
+          matrixElements.length > 3 ||
+          matrixElements.length !== matrixElements.trim().length) {
+        console.log('Incorrect input!');
+        return matrixElementsFn();
+      } else {
+        return matrixElements;
+      }
+    } else {
+      return matrixElementsFn();
+    }
+  }
+  const matrixDimensionFn = () => {
+    const matrixDimension = prompt('Enter a positive number greater than 0 and less than 10.');
+    if (matrixDimension !== null) {
+      if (+matrixDimension < 1 ||
+          +matrixDimension > 9 ||
+          !Number.isInteger(+matrixDimension)) {
+        console.log('Incorrect input!');
+        return matrixDimensionFn();
+      } else {
+        return +matrixDimension;
+      }
+    } else {
+      return matrixDimensionFn();
+    }
+  }
+  const matrixElements = matrixElementsFn();
+  const matrixDimension = matrixDimensionFn();
+  console.log(`${Array(matrixDimension).fill(Array(matrixDimension).fill(matrixElements).join(' ')).join('\n')}`);
+};
+matrix();
