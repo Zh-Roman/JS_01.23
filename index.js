1fecec9c91f3651bbaab5ab7392771ab4fdbcaf5
Roman Zhuravkevich <zhuravkevich.roman@gmail.com>
2023-02-12 18:56:04 +0300

    [4.2:12.02]: finished second task.

--- index.js
+++ index.js
@@ -9,3 +9,40 @@ function curry(fn) {
     }
   };
 }
+
+function isValid(value) {
+  return !(typeof value !== 'number' || !isFinite(value));
+}
+
+class Calculator {
+  constructor(numX, numY) {
+    if (!isValid(numX) || !isValid(numY)) {
+      throw new Error();
+    }
+    this.numX = numX;
+    this.numY = numY;
+  };
+
+  setX = (num) => {
+    if (!isValid(num)) {
+      throw new Error();
+    }
+    this.numX = num;
+  };
+  setY = (num) => {
+    if (!isValid(num)) {
+      throw new Error();
+    }
+    this.numY = num;
+  };
+
+  getSum = () => this.numX + this.numY;
+  getMul = () => this.numX * this.numY;
+  getSub = () => this.numX - this.numY;
+  getDiv = () => {
+    if (this.numY === 0) {
+      throw new Error();
+    }
+    return this.numX / this.numY;
+  };
+}
