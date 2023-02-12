fa067d62a07b44cdaa2f9c4aafc79665fa05804c
Roman Zhuravkevich <zhuravkevich.roman@gmail.com>
2023-02-12 18:57:44 +0300

    [4.3:12.02]: finished third task.

--- index.js
+++ index.js
@@ -46,3 +46,36 @@ class Calculator {
     return this.numX / this.numY;
   };
 }
+
+class RickAndMorty {
+  constructor() {
+    this.baseUrl = 'https://rickandmortyapi.com/api';
+  }
+
+  getCharacter(characterId) {
+    const requestUrl = `${this.baseUrl}/character/${characterId}`;
+    if (!Number.isInteger(characterId) || characterId <= 0) {
+      throw new Error();
+    }
+    return fetch(requestUrl)
+        .then(response => {
+          return response.ok ? response.json() : null;
+        })
+        .catch((e) => {
+          throw new Error(e);
+        });
+  };
+
+  async getEpisode(episodeId) {
+    const requestUrl = `${this.baseUrl}/episode/${episodeId}`;
+    if (!Number.isInteger(episodeId) || episodeId <= 0) {
+      throw new Error();
+    }
+    try {
+      const response = await fetch(requestUrl);
+      return response.ok ? await response.json() : null;
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+}
