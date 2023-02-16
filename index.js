15424e2297d293f95d32e19f9141a8eae1a19baf
Roman Zhuravkevich <zhuravkevich.roman@gmail.com>
2023-02-16 11:17:06 +0300

    [5.2:16.02]: finished second task.

--- index.js
+++ index.js
@@ -50,3 +50,77 @@ class Stack {
     return outputStack;
   };
 }
+
+class ListNode {
+
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  };
+}
+
+class LinkedList {
+
+  constructor() {
+    this.storage = null;
+    this.firstElem = null;
+    this.lastElem = null;
+  };
+
+  append = (elem) => {
+    const newElem = new ListNode(elem);
+    if (!this.storage) {
+      this.firstElem = newElem;
+      this.lastElem = newElem;
+      return this.storage = [newElem];
+    }
+    this.lastElem.next = newElem;
+    this.lastElem = newElem;
+    return this.storage = [...this.storage, newElem];
+  };
+
+  prepend = (elem) => {
+    const newElem = new ListNode(elem, this.firstElem);
+    if (!this.storage) {
+      this.firstElem = newElem;
+      this.lastElem = newElem;
+      return this.storage = [newElem];
+    }
+    this.firstElem = newElem;
+    return this.storage = [newElem, ...this.storage];
+  };
+
+  find = (elem) => {
+    let currentElem = this.firstElem;
+    const findElem = (currNode) => {
+      if (!currNode) {
+        return null;
+      }
+      if (currNode.value === elem) {
+        return currNode;
+      } else {
+        return findElem(currNode.next);
+      }
+    };
+    return findElem(currentElem);
+  };
+
+  toArray = () => !this.storage ? [] : [...this.storage];
+
+  static fromIterable = (iterable) => {
+    if (!iterable[Symbol.iterator]) {
+      throw new Error('Not iterable');
+    }
+    const outputList = new this();
+    if (iterable instanceof Map) {
+      for (let elem of iterable.values()) {
+        outputList.append(elem);
+      }
+    } else {
+      for (let elem of iterable) {
+        outputList.append(elem);
+      }
+    }
+    return outputList;
+  };
+}
