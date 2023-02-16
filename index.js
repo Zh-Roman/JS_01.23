54e1b68868d9d45b9957b5f557554f0b2da70b7a
Roman Zhuravkevich <zhuravkevich.roman@gmail.com>
2023-02-16 11:11:01 +0300

    [5.1:16.02]: finished first task.

--- index.js
+++ index.js
@@ -1,81 +1,52 @@
-function curry(fn) {
-  return function curring(...args) {
-    if (args.length >= fn.length) {
-      return fn.apply(this, args);
+class Stack {
+
+  constructor(limitValue = 10) {
+    if (!Number.isInteger(limitValue) || limitValue < 0) {
+      throw new Error('Invalid limit value');
+    }
+    this.limitValue = limitValue;
+    this.lastElemIndex = 0;
+    this.stackStorage = [];
+  };
+
+  push = (elem) => {
+    if (this.lastElemIndex === this.limitValue) {
+      throw new Error('Limit exceeded');
+    }
+    this.stackStorage[this.lastElemIndex] = elem;
+    this.lastElemIndex++;
+  };
+
+  pop = () => {
+    if (!this.lastElemIndex) {
+      throw new Error('Empty stack');
+    }
+    this.lastElemIndex--;
+    const poppedElem = this.stackStorage[this.lastElemIndex];
+    this.stackStorage.length = this.lastElemIndex;
+    return poppedElem;
+  };
+
+  peek = () => !this.lastElemIndex ? null : this.stackStorage[this.lastElemIndex - 1];
+
+  isEmpty = () => !this.lastElemIndex;
+
+  toArray = () => !this.lastElemIndex ? [] : [...this.stackStorage];
+
+  static fromIterable = (iterable) => {
+    if (!iterable[Symbol.iterator]) {
+      throw new Error('Not iterable');
+    }
+    const outputStack = new this(iterable.size ?? iterable.length);
+    if (iterable instanceof Map) {
+      for (let elem of iterable.values()) {
+        outputStack.push(elem);
+      }
     } else {
-      return function (...nextArgs) {
-        return curring.apply(this, [...args, ...nextArgs]);
-      };
+      for (let elem of iterable) {
+        outputStack.push(elem);
+      }
     }
-  };
-}
-
-function isValid(value) {
-  return !(typeof value !== 'number' || !isFinite(value));
-}
-
-class Calculator {
-  constructor(numX, numY) {
-    if (!isValid(numX) || !isValid(numY)) {
-      throw new Error();
-    }
-    this.numX = numX;
-    this.numY = numY;
-  };
-
-  setX = (num) => {
-    if (!isValid(num)) {
-      throw new Error();
-    }
-    this.numX = num;
-  };
-  setY = (num) => {
-    if (!isValid(num)) {
-      throw new Error();
-    }
-    this.numY = num;
-  };
-
-  getSum = () => this.numX + this.numY;
-  getMul = () => this.numX * this.numY;
-  getSub = () => this.numX - this.numY;
-  getDiv = () => {
-    if (this.numY === 0) {
-      throw new Error();
-    }
-    return this.numX / this.numY;
-  };
-}
-
-class RickAndMorty {
-  constructor() {
-    this.baseUrl = 'https://rickandmortyapi.com/api';
-  }
-
-  getCharacter(characterId) {
-    const requestUrl = `${this.baseUrl}/character/${characterId}`;
-    if (!Number.isInteger(characterId) || characterId <= 0) {
-      throw new Error();
-    }
-    return fetch(requestUrl)
-        .then(response => {
-          return response.ok ? response.json() : null;
-        })
-        .catch((e) => {
-          throw new Error(e);
-        });
-  };
-
-  async getEpisode(episodeId) {
-    const requestUrl = `${this.baseUrl}/episode/${episodeId}`;
-    if (!Number.isInteger(episodeId) || episodeId <= 0) {
-      throw new Error();
-    }
-    try {
-      const response = await fetch(requestUrl);
-      return response.ok ? await response.json() : null;
-    } catch (e) {
-      throw new Error(e);
-    }
+    return outputStack;
   };
 }
